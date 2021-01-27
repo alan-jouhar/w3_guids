67c0c1f35042c095f43c181e88ab1a972bd8af67
alan-jouhar <alan.jouhar@gmail.com>
2021-01-27 11:39:16 +0100

    Added the iconbar component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import IconBar from './components/icon_bar/iconBar'
+import './App.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faHome, faSearch, faEnvelope, faGlobeAmericas, faTrash } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faHome, faSearch, faEnvelope, faGlobeAmericas, faTrash)
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container App">
+      <IconBar> </IconBar>
     </div>
-  );
+  )
 }
+
 
 export default App;
