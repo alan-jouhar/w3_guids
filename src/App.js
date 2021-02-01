8adc000bd93601cd7f5eb3c594d3ab8fa462d746
alan-jouhar <alan.jouhar@gmail.com>
2021-02-01 20:32:05 +0100

    Added the tab header component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import IconBar from './components/icon_bar/iconBar';
-import MenuIcon from './components/menu_icon/menuIcon'
+import MenuIcon from './components/menu_icon/menuIcon';
+import TabHeader from './components/teb_header/tab_header'
 import './App.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faHome, faSearch, faEnvelope, faGlobeAmericas, faTrash } from '@fortawesome/free-solid-svg-icons'
@@ -9,13 +10,30 @@ import Tabs from './components/tabs/tabs'
 library.add(faHome, faSearch, faEnvelope, faGlobeAmericas, faTrash)
 
 function App() {
-
+  let items = [{
+    title: "London",
+    description: "London is the capital city of England.",
+    bg:"green"
+  }, {
+    title: "London",
+    description: "London is the capital city of England.",
+    bg:"blue"
+  }, {
+    title: "London",
+    description: "London is the capital city of England.",
+    bg:"red"
+  }, {
+    title: "London",
+    description: "London is the capital city of England.",
+    bg:"yellow"
+  },]
   return (
     <div className="container App">
       <IconBar />
       <MenuIcon animated />
       <Accordion />
       <Tabs />
+      <TabHeader items={items} />
     </div>
   )
 }
---
+++ src/components/teb_header/tab_header.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import './tab_header.css';
+
+function TabHeader(props) {
+    function activateCard(id) {
+        let cards = document.getElementsByClassName("tab-header__card");
+        Array.prototype.forEach.call(cards, e => {
+            console.log(id, e.id)
+            if (e.id === id) {
+                e.style.display = "block";
+                e.style.backgroundColor = e.getAttribute("data-bg")
+                console.log("inside the if statement")
+            }
+            else {
+                e.style.display = "none"
+            }
+        })
+
+
+    }
+    function activateBtn(e) {
+        let btn = e.target;
+        let btns = document.getElementsByClassName("tab-header__btn");
+        let id = btn.id;
+        activateCard(id);
+        Array.prototype.forEach.call(btns, e => {
+            if (e.style.backgroundColor) {
+                e.style.backgroundColor = ""
+            }
+        })
+        btn.style.backgroundColor = btn.getAttribute("data-bg")
+    }
+
+    return (
+        <div className="tab-header">
+            <div className="tab-header__content">
+                {props.items.map((e, i) => (
+                    <div className="tab-header__card" id={i} data-bg={e.bg}>
+                        <h1>{e.title}</h1>
+                        <p className="tab-header__desc">{e.description}</p>
+                    </div>
+                ))}
+            </div>
+            <div className="tab-header__menu" onClick={activateBtn}>
+                {props.items.map((e, i) => (
+                    <button className="tab-header__btn" data-bg={e.bg} id={i}>{e.title}</button>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default TabHeader;
